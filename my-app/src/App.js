08dc981143a5bdd1c8d2240f02872b50c01a2645
Isaiah Katoboe <katoboeisaiah@gmail.com>
2026-03-17 10:37:29 +1100

    replace default ESLint config

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This i Isaiahs my first React app. I am learning React and I am excited to build amazing things with it!  
         </p>
         <a
           className="App-link"
